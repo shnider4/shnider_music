1b745daa10d66ec9b8263b96a753fd9045792dd1
matrix <64998320+shnider0@users.noreply.github.com>
2021-08-15 20:34:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3 /RadioPlayerV3
+git clone https://github.com/shnider4/shnider_music
 cd /RadioPlayerV3
 echo "Starting Bot, Please Wait..."
 python3 main.py
